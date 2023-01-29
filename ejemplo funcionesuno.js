1ef1976e3cd0d5e1554b7686d6a31a891b1da4ef
Alvaro Pajaro <alvarojavierpajarofernandez@gmail.com>
2023-01-29 06:33:06 -0500

    empeze con ejemplos de funciones

---
+++ ejemplo funcionesuno.js
@@ -0,0 +1,11 @@
+function saludar(){
+    saludo = prompt("Hola alvaro ¿Como estas?");
+        if(saludo == "bien"){
+            alert("que genial amigo");
+        }else{
+            alert("que pena no sabia");
+        }
+}
+saludar();
+saludar();
+saludar();
