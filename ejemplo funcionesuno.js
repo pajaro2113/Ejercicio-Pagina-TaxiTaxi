4160d72cc06df4c3a66ab6813a49d677b98aa08b
Alvaro Pajaro <alvarojavierpajarofernandez@gmail.com>
2023-02-15 05:05:09 -0500

    termine eje entrada discoteca si mayor de edad

--- ejemplo funcionesuno.js
+++ ejemplo funcionesuno.js
@@ -16,28 +16,55 @@
 // }
 // document.write(estadoSaludo());
 
-const sumar = (num1,num2)=>{
-    num1 = parseInt(prompt("primer numero"));
-    num2 = parseInt(prompt("numero dos porfa"));
-    let suma = num1+num2;
-    return suma;
-}
-const restar = (num1,num2)=>{
-    num1 = parseInt(prompt("primer numero a restar"));
-    num2 = parseInt(prompt("numero dos porfavor"));
-    let resta = num1-num2;
-    return resta;
-}
+// const sumar = (num1,num2)=>{
+//     num1 = parseInt(prompt("primer numero"));
+//     num2 = parseInt(prompt("numero dos porfa"));
+//     let suma = num1+num2;
+//     return suma;
+// }
+// const restar = (num1,num2)=>{
+//     num1 = parseInt(prompt("primer numero a restar"));
+//     num2 = parseInt(prompt("numero dos porfavor"));
+//     let resta = num1-num2;
+//     return resta;
+// }
 
-const opcion = (miOpcion)=>{
-    miOpcion = parseInt(prompt("escriba la operacio que desea realizar"));
-    if(miOpcion == 1){
-        return sumar();
-    }else if(miOpcion == 2){
-       return restar();
-    }else{
-        document.write(opcion());
-    }
-    return miOpcion;
-}
-document.write(opcion());
+// const opcion = (miOpcion)=>{
+//     miOpcion = parseInt(prompt("escriba la operacio que desea realizar"));
+//     if(miOpcion == 1){
+//         return sumar();
+//     }else if(miOpcion == 2){
+//        return restar();
+//     }else{
+//         document.write(opcion());
+//     }
+//     return miOpcion;
+// }
+// document.write(opcion());
+// ___________________________________________________
+// let free = false;
+
+// const fiesta = (edad,hora)=>{
+//     edad = parseInt(prompt("escriba su edad"));
+//     hora = parseInt(prompt("escriba la hora que es"));
+//     if(edad >= 18){
+//         if(hora >= 2 && hora <= 6 && free == false){
+//             document.write("puedes pasar GRATIS");
+//             free = true;
+//         }else{
+//             document.write ("puedes pasar pero debes pagar");
+//         }
+//     }else {
+//         document.write("ERES MENOR DE EDAD NO PUEDES PASAR");
+//     }
+// }
+// fiesta();
+// document.write("<br>");
+// fiesta();
+// document.write("<br>");
+// fiesta();
+// ________________________________________________
+
+
+
+
