80a4ef94069d99d6eb0ba806320d73af183a71ba
Alvaro Pajaro <alvarojavierpajarofernandez@gmail.com>
2023-03-29 04:51:30 -0500

    ejemplos con algunas funciones el javascript

--- ejemplouno.js
+++ ejemplouno.js
@@ -73,72 +73,170 @@
 // // ave.verinfo();
 
 
-class celulares {
-    constructor(color,peso,tamaño,resoPantalla,resoCamara,ram){
-        this.color = color;
-        this.peso = peso;
-        this.tamaño = tamaño;
-        this.resoluciondePantalla = resoPantalla;
-        this.resoluciondeCamara = resoCamara;
-        this.memoriaRam = ram;
-        this.encendido = false;
-    }
-    prender(){
-        if (this.encendido == false){
-            alert('el celular esta encendido');
-            this.encendido = true;
-        }else{
-            alert('el telefono esta apagado');
-        }
-    }
-    reiniciar(){
-        if(this.encendido == true){
-            alert('reiniciando celular...');
-        }else{
-            alert('el telefono no se puede reinicial, pues esta apagado');
-        }
-    }
-    tomarFotos(){
-        alert(`foto tomada a una resolucion de ${this.resoluciondeCamara}`);
-    }
-    grabarVideo(){
-        alert(`video grabado a ${this.resoluciondeCamara} Megapixeles`);
-    }
-    mostrarInfo(){
-        return `
-        Color: <b>${this.color}</b></br>
-        Peso: <b>${this.peso}</b></br>
-        Tamaño: <b>${this.tamaño}</b></br>
-        Resolucion de camara: <b>${this.resoluciondePantalla}</b></br>
-        Resolucion de video: <b>${this.resoluciondeCamara}</b></br>
-        Memoria Ram: <b>${this.memoriaRam}</b></br>`;
-    }
-}
+// class celulares {
+//     constructor(color,peso,tamaño,resoPantalla,resoCamara,ram){
+//         this.color = color;
+//         this.peso = peso;
+//         this.tamaño = tamaño;
+//         this.resoluciondePantalla = resoPantalla;
+//         this.resoluciondeCamara = resoCamara;
+//         this.memoriaRam = ram;
+//         this.encendido = false;
+//     }
+//     prender(){
+//         if (this.encendido == false){
+//             alert('el celular esta encendido');
+//             this.encendido = true;
+//         }else{
+//             alert('el telefono esta apagado');
+//         }
+//     }
+//     reiniciar(){
+//         if(this.encendido == true){
+//             alert('reiniciando celular...');
+//         }else{
+//             alert('el telefono no se puede reinicial, pues esta apagado');
+//         }
+//     }
+//     tomarFotos(){
+//         alert(`foto tomada a una resolucion de ${this.resoluciondeCamara}`);
+//     }
+//     grabarVideo(){
+//         alert(`video grabado a ${this.resoluciondeCamara} Megapixeles`);
+//     }
+//     mostrarInfo(){
+//         return `
+//         Color: <b>${this.color}</b></br>
+//         Peso: <b>${this.peso}</b></br>
+//         Tamaño: <b>${this.tamaño}</b></br>
+//         Resolucion de camara: <b>${this.resoluciondePantalla}</b></br>
+//         Resolucion de video: <b>${this.resoluciondeCamara}</b></br>
+//         Memoria Ram: <b>${this.memoriaRam}</b></br>`;
+//     }
+// }
 
-class altaGama extends celulares{
-    constructor(color,peso,tamaño,resoPantalla,resoCamara,ram,grabarLento,rFacial,camExtra){
-        this.color = color;
-        this.peso = peso;
-        this.tamaño = tamaño;
-        this.resoluciondePantalla = resoPantalla;
-        this.resoluciondeCamara = resoCamara;
-        this.memoriaRam = ram;
-        this.encendido = false;
-        this.grabarLento = grabarLento;
-        this.rFacial = rFacial;
-        this.camExtra = camExtra;
-    }
-}
+// class altaGama extends celulares{
+//     constructor(color,peso,tamaño,resoPantalla,resoCamara,ram,camExtra){
+//         super(color,peso,tamaño,resoPantalla,resoCamara,ram);
+//         this.camExtra = camExtra;
+//     }
+//     reconocimientoFaial(){
+//         alert('el telefono esta haciendo uso de reconocimeinto facial');
+//     }
+//     camaraLenta(){
+//         alert('Ahora estamos rabando en camara lenta');
+//     }
+//     mostrarInfo_AltaGama(){
+//         return this.mostrarInfo() + ` Resolucion camara extra: <b>${this.camExtra}</b>`
+//     }
+// }
 
-const celularUno = new celulares("rojo","150g","5'","1024px","724px","4Mb"); 
-const celularDos = new celulares("blanco","10g","15'","2048px","724px","2Mb"); 
-const celularTres = new celulares("gris","250g","10'","1024px","1024px","2Mb"); 
+// const celularUno = new celulares("rojo","150g","5'","1024px","724px","4Mb"); 
+// const celularDos = new celulares("blanco","10g","15'","2048px","724px","2Mb"); 
+// const celularTres = new celulares("gris","250g","10'","1024px","1024px","2Mb"); 
+// const celularUno_AltaGama = new altaGama("rojo","150g","5'","1024px","724px","4Mb","FULL HD");
+// const celularDos_AltaGama = new altaGama("rojo","150g","5'","1024px","724px","4Mb","4K");
 
-// celularUno.prender();
-// celularUno.reiniciar();
-// celularUno.grabarVideo();
-// celularUno.tomarFotos();
-document.write(`
-${celularUno.mostrarInfo()}<br>
-${celularDos.mostrarInfo()}<br>
-${celularTres.mostrarInfo()}<br>`);
+// // celularUno.prender();
+// // celularUno.reiniciar();
+// // celularUno.grabarVideo();
+// // celularUno.tomarFotos();
+// document.write(`
+// ${celularUno.mostrarInfo()}<br>
+// ${celularDos.mostrarInfo()}<br>
+// ${celularTres.mostrarInfo()}<br>
+// ${celularUno_AltaGama.mostrarInfo_AltaGama()}<br><br>
+// ${celularDos_AltaGama.mostrarInfo_AltaGama()}<br>`);
+
+// ---------------------------------------------------------------------------
+
+// class app{
+//     constructor(cantDescargas,puntuacion,peso){
+//         this.cantDescargas = cantDescargas;
+//         this.puntuacion = puntuacion;
+//         this.peso = peso;
+//         this.instalarApp = false;
+//         this.desinstalarApp = false;
+//         this.abirapp = false;
+//         this.cerrarApp = false;
+//     }
+    
+//     listarApp(){
+//         return `Cantidad de descargas: <b>${this.cantDescargas}</b><br>
+//                 Puntuacion de la App: <b>${this.puntuacion}</b><br>
+//                 Peso de  aplicacion: <b>${this.peso}</b><br><br>`;
+//     }
+//     instalada(){
+//         if(this.instalarApp == false){
+//             alert('la aplicacion esta intalada');
+//             this.instalarApp = true;
+//         }
+//     }
+//     desinstalada(){
+//         if(this.instalarApp == true){
+//             alert('la aplicacion ha sido desinstaada');
+//         }else if (this.instalarApp = false) {
+//             alert('la aplicacion o existe en el dispositivo');
+//         }
+//     }
+//     abierta(){
+//         if(this.abirapp == false && this.instalarApp == true){
+//             alert('Aplicacion abierta');
+//             this.abirapp = true;
+//         }else{
+//             alert('la app no se pudo abrir por qe ya esta abierta o por qe no esta instalada');
+            
+//         }
+//     }
+//     cerada(){
+//         if(this.abirapp == true && this.instalarApp == true){
+//             alert('Aplicacion ha sido cerrada');
+//         }else{
+//             alert('la app o se pudo cerrar por qe ya esta cerrada o por qe no esta instalada');
+            
+//         }
+//     }
+// }
+// let dato1 = parseInt(prompt('escriba la cantidad de descargas...'));
+// const appUno = new app(dato1,"6.3","40Mb");
+// const appDos = new app("10","5.3","40Mb");
+// const appTes = new app("1","5.0","60Mb");
+// const appCuatro = new app("47","8.0","30Mb");
+// const appCinco = new app("10","3.8","70Mb");
+// const appSeis = new app("70","9.9","10Mb");
+// const appSiete = new app("10","15.3","20Mb");
+
+// appUno.instalada();
+// appUno.desinstalada();
+// appUno.cerada();
+
+// document.write(`${appUno.listarApp()}`);
+// document.write(`${appDos.listarApp()}`);
+// document.write(`${appTes.listarApp()}`);
+// document.write(`${appCuatro.listarApp()}`);
+// document.write(`${appCinco.listarApp()}`);
+// document.write(`${appSeis.listarApp()}`);
+// document.write(`${appSiete.listarApp()}`);
+
+
+//______________________________________________________________________________
+//CADENAS
+
+let cadena1 = "cadenadeprueba";
+let cadena2 = "prueba"
+
+//resultado = cadena1.concat(cadena2); 
+//concat junta dos o mas cadenas
+
+// resultado = cadena1.startsWith(cadena2);
+// resultado = cadena1.endsWith(cadena2);
+// startsWith o endswith-> si una cadena comienza o termina con los caracteres de la otra muestra TRUE sino FALSE
+
+// resultado = cadena1.includes(cadena2);
+// includes -> si lo qe esta en al cadena dos pertenece a la cadena uno;
+
+resultado = cadena1.substring(0,2);
+//substring -> muestra solo los caracteres desde el que empieza hasta el que termina -1;
+
+
+document.write(resultado);
